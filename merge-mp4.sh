d66c4103a7e507dbc6b3efa9b97ce0d20cc20203
wachin <wachin.id@gmail.com>
2022-04-19 11:40:29 -0500

    I add the script merge-mp4.sh developed by palaniraja to join mp4 and I update rec.sh

---
+++ merge-mp4.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+## Script to merge all mp4 videos in current directory (recursively 2 levels)
+## And update chapter marks to retain the folder/filename
+
+## Script for merging videos
+
+
+filename=`basename pwd`
+current=`pwd`
+bname=`basename "$current"`
+find . -maxdepth 2 -iname '*.mp4' | xargs -L 1 echo | awk '{printf "file \x27%s\x27\n", $0}' >> list.txt
+find . -maxdepth 2 -iname '*.mp4' | xargs -L 1 echo | awk '{print $0}' >> files.txt
+echo -n "Merging the files"
+ffmpeg -f concat -safe 0 -i list.txt -c copy "$bname.mp4" -v quiet
+echo "..........[ DONE ]"
+
+## extract meta
+# ffmpeg -i all.mp4 -f ffmetadata metafile
+metafile="metadata.txt"
+echo -n "Extracting meta data"
+ffmpeg -i "$bname.mp4" -f ffmetadata $metafile -v quiet
+echo "..........[ DONE ]"
+
+## chapter marks
+#TODO: (‘=’, ‘;’, ‘#’, ‘\’) to be escaped
+ts=0
+echo -n "Identifying chapters"
+cat files.txt | while read file
+do
+    ds=`ffprobe -v quiet -of csv=p=0 -show_entries format=duration "$file"`
+    # echo "$ds"
+    echo "[CHAPTER]" >> $metafile
+    echo "TIMEBASE=1/1" >> $metafile
+    echo "START=$ts" >> $metafile
+    ts=`echo $ts + $ds | bc`
+    echo "END=$ts" >> $metafile
+    echo "TITLE=$file" >> $metafile
+    
+done
+echo "..........[ DONE ]"
+## update meta with chaptermarks
+
+echo -n "Adding chapter meta "
+
+ffmpeg -i "$bname.mp4" -i $metafile -map_metadata 1 -codec copy "$bname-meta.mp4" -v quiet
+echo "..........[ DONE ]"
+
+## cleanup
+echo -n "Cleaning up"
+
+rm files.txt list.txt $metafile
+
+echo "..........[ DONE ]"
+
+echo "Job Completed."
--- rec.sh
+++ rec.sh
@@ -2,7 +2,10 @@
 
 set -x
 
-## SALUDOS EN ESTE TUTORIAL
+## Otras maneras de grabar con ffmpeg en:
+## More ways to record with ffmpeg in:
+## [Screencast] Grabar pantalla con FFmpeg 4.2 + seguir cursor + mostrar cursor + grabar rectangulo de 480p 
+## https://facilitarelsoftwarelibre.blogspot.com/2021/03/screencast-ffmpeg-follow-mouse-linux.html
 
 ffmpeg -follow_mouse 50 -show_region 1 -video_size 854x480 -f 30 -f x11grab -i :0.0  \
        -f alsa -ac 2 -i default  \
